692d9c0deab31d41f232f7e6b69bbc51889b03a0
umar <qarshiyevumar@gmail.com>
2025-07-17 16:45:06 +0500

    employee+attachment version 5

--- src/main/java/uz/pdp/startupproject/payload/EmployeeDTO.java
+++ src/main/java/uz/pdp/startupproject/payload/EmployeeDTO.java
@@ -1,9 +1,13 @@
 package uz.pdp.startupproject.payload;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import uz.pdp.startupproject.entity.Employee;
 import uz.pdp.startupproject.enums.Gender;
 import uz.pdp.startupproject.enums.RoleEnum;
+import uz.pdp.startupproject.payload.withoutId.UserDto;
 
 import java.io.Serializable;
 
@@ -14,15 +18,24 @@ import java.io.Serializable;
 @Builder
 public class EmployeeDTO implements Serializable {
     private Long id;
+    @NotBlank(message = "firstName bo'sh bo'lishi mumkin emas")
     private String firstName;
+
+    @NotBlank(message = "lastName bo'sh bo'lishi mumkin emas")
     private String lastName;
+
+    @NotBlank(message = "phoneNumber bo'sh bo'lishi mumkin emas")
+    @Pattern(regexp = "\\+998\\d{9}",message = "tel nomer formati noto'g'ri Masalan: +998901234567")
     private String phoneNumber;
+
+    @NotNull(message = "gender bo'sh bo'lishi mumkin emas")
     private Gender gender;
 
     private UserDTO user;
 
+    @NotNull(message = "attachmentId bo'sh bo'lishi mumkin emas")
     private Long attachmentId;
 
+    @NotNull(message = "companyId bo'sh bo'lishi mumkin emas")
     private Long companyId;
-
 }
--- src/main/java/uz/pdp/startupproject/payload/UserDTO.java
+++ src/main/java/uz/pdp/startupproject/payload/UserDTO.java
@@ -1,6 +1,8 @@
 package uz.pdp.startupproject.payload;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import uz.pdp.startupproject.enums.RoleEnum;
 
@@ -14,8 +16,17 @@ import java.io.Serializable;
 public class UserDTO implements Serializable {
 
     private Long id;
+
+
+    @NotBlank(message = "userName bush bulishi mumkun emas!")
     private String username;
+
+    @NotBlank(message = "password bush bulishi mumkun emas!")
+    @Size(min = 4, message = "password kamida 4 ta belgi bulishi kerak")
     private String password;
+
+
 //    private String email;
+
     private RoleEnum role;
 }
